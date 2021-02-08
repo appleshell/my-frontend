94924a3a0a265c2379b056b857229902718a9687
ps <appleshell@outlook.com>
2021-02-08 18:40:41 +0800

    update blog

---
+++ bin/a622c65b2c713c890fcf4fbf07cf34049d5fe758/out/vs/workbench/services/files/node/watcher/unix/watcherApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/services/files/node/watcher/unix/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/a622c65b2c713c890fcf4fbf07cf34049d5fe758/core/vs/workbench/services/files/node/watcher/unix/watcherApp.nls.js.map
